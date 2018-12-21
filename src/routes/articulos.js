aee39b85c0c6b8f99ed34e6cab9595344d4f592e
Adrian Cecilia <adrian.cecilia4@gmail.com>
2018-12-21 10:53:19 +0100

    Create Print Secction
    Create print seccion and renew config

--- src/routes/articulos.js
+++ src/routes/articulos.js
@@ -105,6 +105,54 @@ router.put('/articulos/edit-articulo/:id', async (req, res) => {
     res.redirect('/articulos');
 });
 
+
+// IMPRIMIR ARTICULO
+
+router.get('/print/:id', async (req, res) => {
+    const articulo = await Articulo.findById(req.params.id);
+    res.render('print', {
+        articulo
+    });
+});
+
+router.put('print/:id', async (req, res) => {
+    const {
+        numObra,
+        tipo,
+        autor,
+        titulo,
+        realizado,
+        procedimiento,
+        medidas,
+        procedencia,
+        precio,
+        catalogo,
+        ubicacion,
+        descripcion,
+        notas,
+        img
+    } = req.body;
+    await Articulo.findByIdAndUpdate(req.params.id, {
+        numObra,
+        tipo,
+        autor,
+        titulo,
+        realizado,
+        procedimiento,
+        medidas,
+        procedencia,
+        precio,
+        catalogo,
+        ubicacion,
+        descripcion,
+        notas,
+        img
+    });
+    req.flash('success_msg', 'Ficha Mandada a Imprimir');
+    res.redirect('/articulos');
+});
+
+
 // ELIMINAR LOS ARTICULOS
 router.delete('/articulos/delete/:id', async (req, res) => {
     await Articulo.findByIdAndDelete(req.params.id);
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,4 @@ router.get('/help', (req, res) => {
     res.render('help');
 })
 
-router.get('/print/:id', (req, res) => {
-    res.render('print');
-})
-
 module.exports = router;
